64f57259d22ce362c7b15529feca2c84656604ed
zaynlannister <zaynlannister@gmail.com>
2023-12-22 00:34:01 +0500

    feat: add sorting button

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -4,3 +4,7 @@ export interface Post {
   title: string
   body: string
 }
+
+export interface Sorting {
+  [key: string]: any
+}
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+    <div class="sort-content">
+      <button @click="sortByTitle">{{ sortLabel }}</button>
+    </div>
     <div>
       <SinglePost v-for="post in posts" :key="post.id" :post="post" @on-view="openPost(post)" />
     </div>
@@ -12,28 +15,47 @@
 <script setup lang="ts">
 import { $api } from '@/api'
 import SinglePost from '@/components/SinglePost.vue'
-import type { Post } from '@/interfaces'
-import { computed, ref } from 'vue'
+import type { Post, Sorting } from '@/interfaces'
+import { computed, ref, watch } from 'vue'
 import { useRouter } from 'vue-router'
 
 const router = useRouter()
 const page = ref<number>(1)
 const pageSize = ref<number>(5)
 const posts = ref<Post[]>([])
+const sorting = ref<Sorting>({})
 
-const { isFetching, execute, onFetchResponse } = $api(
-  () => `/posts?page=${page.value}&_limit=${pageSize.value}`
-)
+const hasSorting = computed(() => Object.keys(sorting.value).length > 0)
+
+const queryParams = computed(() => {
+  const params: any = {
+    _page: page.value,
+    _limit: pageSize.value
+  }
+
+  for (const key in sorting.value) {
+    params[key] = sorting.value[key]
+  }
+
+  return '?' + new URLSearchParams(params as any)
+})
+
+const { isFetching, execute, onFetchResponse } = $api(() => `/posts${queryParams.value}`)
   .get()
   .json<Post[]>()
 
 onFetchResponse(async (data) => {
   const response = (await data.json()) as Post[]
 
+  if (hasSorting.value && page.value === 1) {
+    posts.value = []
+  }
+
   posts.value.push(...response)
 })
 
 const loadMoreLabel = computed(() => (isFetching.value ? 'Loading...' : 'Load more'))
+const sortLabel = computed(() => (hasSorting.value ? 'Sort by default' : 'Sort by title'))
 
 function openPost(post: Post) {
   router.push({
@@ -48,10 +70,25 @@ function loadMore() {
   page.value++
   execute()
 }
+
+function sortByTitle() {
+  sorting.value = {
+    _sort: 'title'
+  }
+}
+
+watch(sorting, () => {
+  page.value = 1
+  execute()
+})
 </script>
 
 <style scoped lang="scss">
 .load-content {
   padding: 10px 0 40px 0;
+}
+
+.sort-content {
+  padding-bottom: 10px;
 }
 </style>
