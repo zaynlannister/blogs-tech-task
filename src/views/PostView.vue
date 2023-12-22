fc6f7bfe45e81d89dd53005be256d9e8b48d5ae4
zaynlannister <zaynlannister@gmail.com>
2023-12-22 18:00:17 +0500

    refactor: style post view page

--- src/views/PostView.vue
+++ src/views/PostView.vue
@@ -1,14 +1,51 @@
 <template>
-  <div>post page {{ $route.params }}</div>
-  <div>{{ data }}</div>
+  <div class="post">
+    <div>
+      <ArrowIcon @click="goToTheMain" />
+    </div>
+    <div class="post__content" v-if="!isFetching">
+      <p><span class="post__title">Title:</span> {{ data?.title }}</p>
+      <p class="post__body-container">
+        <span class="post__body">Description:</span> {{ data?.body }}
+      </p>
+    </div>
+    <div v-else>Loading...</div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { $api } from '@/api'
-import { useRoute } from 'vue-router'
+import type { Post } from '@/interfaces'
+import { useRoute, useRouter } from 'vue-router'
+import ArrowIcon from '@/icons/ArrowIcon.vue'
 
 const route = useRoute()
-const { data } = $api(`/posts/${route.params.id}`)
+const router = useRouter()
+const { data, isFetching } = $api(`/posts/${route.params.id}`).get().json<Post>()
+
+function goToTheMain() {
+  router.push({
+    path: '/'
+  })
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.post {
+  &__content {
+    border: 1px solid silver;
+    padding: 20px 10px;
+    margin-top: 20px;
+    border-radius: 4px;
+  }
+
+  &__title,
+  &__body {
+    font-weight: bold;
+  }
+
+  &__body-container {
+    margin-top: 10px;
+  }
+}
+</style>
